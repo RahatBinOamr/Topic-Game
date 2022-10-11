8743e651bf53c64ebd0c849420822abf474ab286
RahatBinOamr <108364543+RahatBinOamr@users.noreply.github.com>
2022-10-11 07:32:43 +0600

    added question from blog component How does context api work and answer

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -4,14 +4,28 @@ const Blog = () => {
     return (
         <div>
             <div>
-            <h1 className='text-2xl text-white font-semibold mb-5'> What is the purpose of react router? </h1>
-        <p className=' text-white font-semibold mb-5'>
-            React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL.
-            Let us create a simple application to React to understand how the React Router works. The application will contain three components: home component, about a component, and contact component. We will use React Router to navigate between these components.
+                <h1 className='text-2xl text-white font-semibold mb-5'>    What is the purpose of react router? </h1>
+                <p className=' text-white font-semibold mb-5'>
+                    React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL.
+                    Let us create a simple application to React to understand how the React Router works. The application will contain three components: home component, about a component, and contact component. We will use React Router to navigate between these components.
 
-            Setting up the React Application: Create a React application using create-react-app and lets call it geeks.
-        </p>
-            </div>
+                    Setting up the React Application: Create a React application using create-react-app and lets call it geeks.
+                </p>
+        </div>
+        <div>
+            <h1 className='text-2xl text-white font-semibold mb-5'>
+                How does Context API Work
+            </h1>
+            <p className=' text-white font-semibold mb-5 text-left p-2'>
+            There are four steps to using react context <br />
+            1. Create context using the create context method <br />
+            2. Take Your created context and worp the context provider around your component tree <br />
+            3. put any value you like on your context provider using the value prop. <br />
+            4. Read that value within any component by using useContext.
+
+            
+            </p>
+        </div>
         </div>
     );
 };
